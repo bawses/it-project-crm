10c1f5193b80ba0ce3b9afd8eb416bd711f89d51
ilaylow <chuenleylow@gmail.com>
2021-08-15 18:21:23 +1000

    backend test fail #1

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-import Next from "next";
+import next from "next";
 import express from "express";
 
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3000;
 const app = next({dev});
 
-app.prepare.then(() => {
+app.prepare().then(() => {
     const server = express();
 
     server.listen(port, err => {
