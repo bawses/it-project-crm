ddc6bb1b10be659177a72e693c03180f05f38612
ilaylow <chuenleylow@gmail.com>
2021-08-15 17:55:14 +1000

    configuring backend test

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import Next from "next";
+import express from "express";
+
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || 3000;
+const app = next({dev});
+
+app.prepare.then(() => {
+    const server = express();
+
+    server.listen(port, err => {
+
+        if (err) throw err;
+        console.log(`Listening on PORT ${port}`);
+    })
+})
