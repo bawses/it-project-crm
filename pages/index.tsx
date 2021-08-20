e305e6c66a62005d04fe908cb2a12b0ff2db4b80
Ehsan Soltani Abhari <esoltaniabha@student.unimelb.edu.au>
2021-08-21 02:43:25 +1000

    test commit to check CI-CD

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js! TEST!!!!</a>
         </h1>
 
         <br />
