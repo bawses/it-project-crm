4d910bb83ea79ad73a58652d98913ec8647cae26
Ehsan Soltani Abhari <esoltaniabha@student.unimelb.edu.au>
2021-08-22 00:09:45 +1000

    Confirming that CI/CD is working correctly for PR

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! TEST 12345?!!??</a>
+          Welcome to <a href="https://nextjs.org">Next.js! TEST 78915?!!??</a>
         </h1>
 
         <br />
