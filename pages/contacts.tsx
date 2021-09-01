f18350f491a4e116d0d6cec4aa7b705bb011cc2b
Ehsan Soltani Abhari <esoltaniabha@student.unimelb.edu.au>
2021-09-01 12:13:55 +1000

    Partially complete contacts page, missing tag system and proper resizing functionality

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,8 +1,10 @@
+import { useMediaQuery } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import ContactsTable from '../components/contactsTable';
 import ContactsTableCategory from '../components/contactsTableCategory';
 import { ContactsTableRowProps } from '../components/contactsTableRow';
 import ContactsTableSort from '../components/contactsTableSort';
+import theme from '../src/theme';
 
 const tempContacts: ContactsTableRowProps[] = [
   { name: "John Appleseed", role: "Engineer at F", isStarred: false },
@@ -11,9 +13,11 @@ const tempContacts: ContactsTableRowProps[] = [
 ]
 
 export default function Contacts() {
+  //console.log(useMediaQuery(theme.breakpoints.up("lg")))
+
   return (
     <Box>
-      <Box display="flex" flexDirection="column" justifyContent="centre" mx={20} my={5}>
+      <Box display="flex" flexDirection="column" justifyContent="centre" mx="10%" my="5%">
         {/* Entire table, including filters and tags */}
         <Box>
           {/* Tags */}
