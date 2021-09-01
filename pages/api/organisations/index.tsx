5f154ec6087bb52fee62eaa09ff94d20a4747429
Tony Dang <tungtonydang@gmail.com>
2021-09-01 15:42:24 +1000

    Refactoring apiHandlers.tsx

--- pages/api/organisations/index.tsx
+++ pages/api/organisations/index.tsx
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { indexHandler } from "../../../backend/lib/apiHandler";
-import Organisation from "../../../backend/models/Organisation";
+import { DataType } from "../../../components/DataTypes";
 
 export default async function handler(req: Request, res: Response): Promise<void> {
-  return await indexHandler(req, res, Organisation);
+  return await indexHandler(req, res, DataType.Organisation);
 }
