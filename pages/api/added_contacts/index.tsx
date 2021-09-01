2548987ae2e7eb8bc133ad989000a6983f7df283
Tony Dang <tungtonydang@gmail.com>
2021-09-01 15:33:39 +1000

    Added DbMapping.tsx, DataTypes.tsx

--- pages/api/added_contacts/index.tsx
+++ pages/api/added_contacts/index.tsx
@@ -3,5 +3,5 @@ import { indexHandler } from "../../../backend/lib/apiHandler";
 import AddedContact from "../../../backend/models/AddedContact";
 
 export default async function handler(req: Request, res: Response): Promise<void> {
-  return indexHandler(req, res, AddedContact);
+  return await indexHandler(req, res, AddedContact);
 }
