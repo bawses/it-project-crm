51901a9993fe756433af688cae953fcbbde1a1ab
Tony Dang <tungtonydang@gmail.com>
2021-09-01 15:42:24 +1000

    Refactoring apiHandlers.tsx

--- pages/api/users/[id].tsx
+++ pages/api/users/[id].tsx
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { idHandler } from "../../../backend/lib/apiHandler";
-import User from "../../../backend/models/User";
+import { DataType } from "../../../components/DataTypes";
 
 export default async function handler(req: Request, res: Response): Promise<void> {
-  return await idHandler(req, res, User);
+  return await idHandler(req, res, DataType.User);
 }
