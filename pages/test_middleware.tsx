cafe50db798df9f840c3a32f23590c132016fe64
Tony Dang <tungtonydang@gmail.com>
2021-09-04 00:05:11 +1000

    Updating data interfaces, added ContactQueries.tsx, test_middleware.tsx

---
+++ pages/test_middleware.tsx
@@ -0,0 +1,9 @@
+import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from "../middleware/UserQueries";
+
+export default function TestMiddleware() {
+  return (
+    <div>
+      <button></button>
+    </div>
+  );
+}
