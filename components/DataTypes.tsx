fadf2aacec527b93717bbd118990b7ea918089ea
Tony Dang <tungtonydang@gmail.com>
2021-09-05 17:25:12 +1000

    Fix data types

--- components/DataTypes.tsx
+++ components/DataTypes.tsx
@@ -5,24 +5,25 @@ export enum DataType {
   AddedContact = "added_contact",
 }
 
-type UserId = string;
-type OrganisationId = string;
+type ObjectId = string;
+type UserId = ObjectId;
+type OrganisationId = ObjectId;
 
 export interface IAddedContact {
-  _id?: string;
+  _id?: ObjectId;
   fromUserId: UserId;
   toUserId: UserId;
   notes?: string;
-  tags?: [string];
+  tags?: string[];
   starred?: boolean;
 }
 
 export interface IManualContact {
-  _id?: string;
+  _id?: ObjectId;
   ownerId: UserId;
   name: { firstName: string; lastName: string };
-  email?: [string];
-  phone?: [string];
+  email?: string[];
+  phone?: string[];
   job?: string;
   location?: string;
   links?: {
@@ -31,20 +32,20 @@ export interface IManualContact {
     instagram?: string;
     twitter?: string;
     website?: string;
-    other?: [string];
+    other?: string[];
   };
   notes?: string;
-  tags?: [string];
+  tags?: string[];
   starred?: boolean;
   archived?: boolean;
 }
 
 export interface IOrganisation {
-  _id?: string;
+  _id?: OrganisationId;
   passwordHash: string;
   name: string;
-  email: [string];
-  phone?: [string];
+  email: string[];
+  phone?: string[];
   location?: string;
   links?: {
     facebook?: string;
@@ -52,19 +53,19 @@ export interface IOrganisation {
     instagram?: string;
     twitter?: string;
     website?: string;
-    other?: [string];
+    other?: string[];
   };
   industry?: string;
   about?: string;
-  contacts?: [UserId];
+  contacts?: UserId[];
 }
 
 export interface IUser {
-  _id?: string;
+  _id?: UserId;
   passwordHash: string;
   name: { firstName: string; lastName: string };
-  email: [string];
-  phone?: [string];
+  email: string[];
+  phone?: string[];
   job?: string;
   location?: string;
   links?: {
@@ -73,11 +74,11 @@ export interface IUser {
     instagram?: string;
     twitter?: string;
     website?: string;
-    other?: [string];
+    other?: string[];
   };
   about?: string;
-  allTags?: [string];
-  organisations?: [OrganisationId];
+  allTags?: string[];
+  organisations?: OrganisationId[];
 }
 
 export type DataInterface = IUser | IOrganisation | IManualContact | IAddedContact;
