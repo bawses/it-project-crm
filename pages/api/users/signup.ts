1070dfce449e58afd4a7580bea2f983cb8ac1c99
Ley Low <chuenleylow@gmail.com>
2021-09-10 03:49:54 +1000

    api handler use fixed for dev

--- pages/api/users/signup.ts
+++ pages/api/users/signup.ts
@@ -1,26 +1,15 @@
-import { hash as hashPassword } from "bcryptjs";
 import { Request, response, Response } from "express";
 import { indexHandler } from "../../../backend/ApiHandlers";
 import { DataType } from "../../../lib/DataTypes";
-
-function validateCreateObject(req: Request) : boolean{
-  const {email, passwordHash} = req.body;
-  if (!email || !email.includes("@") || !passwordHash || passwordHash.trim().length < 7) {
-    return false;
-  }
-  return true;
-}
 
 async function handler(req: Request, res: Response): Promise<any> { // Change any type later
   // SignUp method has to be of method POST
   if (req.method !== "POST") {
     res.status(422).json({ error: "Request method is not POST" });
   }
-  // Hash Password
-  const hashedPassword = await hashPassword(req.body.passwordHash, 10);
-  console.log(`After Hash Password: ${hashedPassword}`);
-  req.body.passwordHash = hashedPassword;
-  return await indexHandler(req, res, DataType.User, validateCreateObject);
+
+  let response = await indexHandler(req, res, DataType.User);
+  return response;
 }
 
 export default handler;
