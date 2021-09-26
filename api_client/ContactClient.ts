d8488f0bc3c598d860d06a3b2c1fb57c695cdc2b
Tony Dang <tungtonydang@gmail.com>
2021-09-26 13:18:39 +1000

    New: add to list and remove from list functionality

--- api_client/ContactClient.ts
+++ api_client/ContactClient.ts
@@ -143,43 +143,30 @@ export const toggleArchiveContact = async (contact: IContact): Promise<IContact>
   return updateContact_Manual(contact._id, { archived: !contact.archived });
 };
 
+export const addFieldToContact = async (contact: IContact, fieldObj: Object): Promise<IContact> => {
+  return updateContact(contact, { $addToSet: fieldObj });
+};
+
+export const removeFieldFromContact = async (
+  contact: IContact,
+  fieldObj: Object
+): Promise<IContact> => {
+  return updateContact(contact, { $pull: fieldObj });
+};
+
 export const addTagToContact = async (contact: IContact, tag: string): Promise<IContact> => {
-  let updateObj = { $addToSet: { tags: tag } };
-  if (!contact.isManualContact && !contact.isAddedContact) {
-    throw new Error("Not a manual contact or added contact. Cannot add tag.");
-  }
-  let newObj;
-  if (contact.isManualContact) {
-    let manualContact = await updateManualContact(contact._id, updateObj);
-    newObj = convert_ManualContact_to_Contact(manualContact);
-  } else {
-    let addedContact = await updateAddedContact(contact._id, updateObj);
-    let user = await getUserById(contact._id);
-    newObj = convert_AddedUser_to_Contact(addedContact, user);
-  }
+  let newObj = await addFieldToContact(contact, { tags: tag });
   await updateUser({ $addToSet: { allTags: tag } });
   return newObj;
 };
 
 export const removeTagFromContact = async (contact: IContact, tag: string): Promise<IContact> => {
-  let updateObj = { $pull: { tags: tag } };
-  if (!contact.isManualContact && !contact.isAddedContact) {
-    throw new Error("Not a manual contact or added contact. Cannot remove tag.");
-  }
-  let newObj;
-  if (contact.isManualContact) {
-    let manualContact = await updateManualContact(contact._id, updateObj);
-    newObj = convert_ManualContact_to_Contact(manualContact);
-  } else {
-    let addedContact = await updateAddedContact(contact._id, updateObj);
-    let addedUser = await getUserById(contact._id);
-    newObj = convert_AddedUser_to_Contact(addedContact, addedUser);
-  }
-
+  let newObj = await removeFieldFromContact(contact, { tags: tag });
   // See if this tag still exists in the user's contacts, otherwise remove it from all tags
   let searchObj = { tags: tag };
   let a = await searchAddedContacts(searchObj);
   let b = await searchManualContacts(searchObj);
+  console.log(a, b);
   if (!a.length && !b.length) {
     await updateUser({ $pull: { allTags: tag } });
   }
--- pages/test_apis/contacts.tsx
+++ pages/test_apis/contacts.tsx
@@ -16,7 +16,7 @@ import { getAllTags } from "../../api_client/UserClient";
 export default function TestContactApis() {
   async function test() {
     try {
-      let manualContact = {
+      let mContact = {
         name: {
           firstName: "Tony Test",
           lastName: "Dang",
@@ -25,56 +25,64 @@ export default function TestContactApis() {
       };
 
       console.log("Testing create manual contact");
-      let testCreateManual = await createContact_Manual(manualContact);
-      console.log(testCreateManual);
+      let manualContact = await createContact_Manual(mContact);
+      console.log(manualContact);
 
       const id: string = "614c7d7ce45a3d36b09a6a5a";
       console.log("Testing add user contact");
-      let testAddContact = await addContact_User(id);
-      console.log(testAddContact);
+      // let addedUser = await addContact_User(id);
+      // console.log(addedUser);
 
       console.log("Testing get all contacts");
-      const contacts = await getContacts();
+      let contacts = await getContacts();
       console.log(contacts);
 
       console.log("Testing get added user");
-      let testGetAddedUser = await getContact(id, false);
-      console.log(testGetAddedUser);
+      let addedUser = await getContact(id, false);
+      console.log(addedUser);
 
       console.log("Testing update added user");
-      let testUpdateAddedUser = await updateContact(testGetAddedUser, {
+      addedUser = await updateContact(addedUser, {
         notes: "THIS IS UPDATED KABOOP",
       });
-      console.log(testUpdateAddedUser);
+      console.log(addedUser);
 
       console.log("Testing star added user");
-      let testStar = await toggleStarContact(testUpdateAddedUser);
-      console.log(testStar);
-      testStar = await toggleStarContact(testStar);
-      console.log(testStar);
+      addedUser = await toggleStarContact(addedUser);
+      console.log(addedUser);
+      addedUser = await toggleStarContact(addedUser);
+      console.log(addedUser);
 
       console.log("Testing archive manual contact");
-      let testArchive = await toggleArchiveContact(testCreateManual);
-      console.log(testArchive);
-      testArchive = await toggleArchiveContact(testArchive);
-      console.log(testArchive);
+      manualContact = await toggleArchiveContact(manualContact);
+      console.log(manualContact);
+      manualContact = await toggleArchiveContact(manualContact);
+      console.log(manualContact);
 
       console.log("Testing search contacts");
-      let testSearch = await searchContactsByName("Dang");
-      console.log(testSearch);
+      let searchResult = await searchContactsByName("Dang");
+      console.log(searchResult);
 
       console.log("Testing tags");
-      let testTag = await addTagToContact(testCreateManual, "TEST TAG");
-      console.log(testTag);
+      manualContact = await addTagToContact(manualContact, "TEST TAG");
+      console.log(manualContact);
       console.log(await getAllTags());
-      testTag = await removeTagFromContact(testCreateManual, "TEST TAG");
-      console.log(testTag);
+      addedUser = await addTagToContact(addedUser, "TEST TAG");
+      console.log(addedUser);
+      console.log(await getAllTags());
+      manualContact = await removeTagFromContact(manualContact, "TEST TAG");
+      console.log(manualContact);
+      console.log(await getAllTags());
+      addedUser = await removeTagFromContact(addedUser, "TEST TAG");
+      console.log(addedUser);
       console.log(await getAllTags());
 
       console.log("Testing delete");
-      await deleteContact(testGetAddedUser);
-      await deleteContact(testCreateManual);
+      await deleteContact(manualContact);
+      await deleteContact(addedUser);
       console.log("Delete success");
+
+      console.log(await getAllTags());
 
       console.log("Test success");
     } catch (err) {
