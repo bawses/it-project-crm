edbb6d9598d08cda18ef28d8d4d210af9d2f71d5
Ley Low <chuenleylow@gmail.com>
2021-10-06 16:24:07 +1100

    fixed button on sign up page

--- next.config.js
+++ next.config.js
@@ -12,7 +12,9 @@ module.exports = {
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    CLOUDINARY_URL: process.env.CLOUDINARY_URL
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
   },
   images: {
     domains: ['res.cloudinary.com', 'lh3.googleusercontent.com'],
