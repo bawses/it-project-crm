fae16d4bb18637f22e40ca9db53b0f25f021bb01
“deborahlam123” <deborahlam123@gmail.com>
2021-10-22 18:29:57 +1100

    Redirect to landing page at root URL

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         source: '/',
-        destination: '/login',
+        destination: '/welcome',
         permanent: true,
       },
     ]
---
+++ pages/welcome/index.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect, useCallback } from "react";
+import { useRouter } from "next/router";
+import PageLoadingBar from "../../components/PageLoadingBar";
+import { getSession } from "next-auth/client";
+import WelcomeSection from "../../components/landingSections/WelcomeSection";
+import ProfessionalsSection from "../../components/landingSections/ProfessionalsSection";
+import OrganisationsSection from "../../components/landingSections/OrganisationsSection";
+
+export default function Welcome() {
+  const router = useRouter();
+
+  // If user already logged in, redirect to logged in home page (contacts)
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace('/contacts');
+      }
+    });
+  }, [router]);
+
+  return (
+<div>
+    <WelcomeSection />
+    <ProfessionalsSection />
+    <OrganisationsSection />
+</div>
+  );
+}
