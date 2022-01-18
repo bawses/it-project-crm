b563e09ad93dac7d30092a4422a66b5b3a9737e9
Ehsan Soltani Abhari <esoltaniabha@student.unimelb.edu.au>
2022-01-18 17:29:58 +1100

    Added custom header to welcome page using react-helmet so that thumbnail displays properly when linked

--- pages/welcome/index.tsx
+++ pages/welcome/index.tsx
@@ -5,6 +5,7 @@ import { getSession } from "next-auth/client";
 import WelcomeSection from "../../components/landingSections/WelcomeSection";
 import ProfessionalsSection from "../../components/landingSections/ProfessionalsSection";
 import OrganisationsSection from "../../components/landingSections/OrganisationsSection";
+import { Helmet } from "react-helmet";
 
 export default function Welcome() {
   const router = useRouter();
@@ -24,20 +25,35 @@ export default function Welcome() {
   }, []);
 
   return (
-    <div>
-      <WelcomeSection
-        onPressCTA={() => router.push(isLoggedIn ? "/contacts" : "/login")}
-        CTAtitle={isLoggedIn ? "Go to your contacts" : "Login"}
-        btnLoading={btnLoading}
-      />
-      <ProfessionalsSection
-        isLoggedIn={isLoggedIn}
-        onPressCTA={() => router.push("/signup")}
-      />
-      <OrganisationsSection
-        isLoggedIn={isLoggedIn}
-        onPressCTA={() => router.push("/signup")}
-      />
-    </div>
+    <>
+      <Helmet>
+        <meta name="description" content="CataLog CRM" />
+        <meta name="title" property="og:title" content="CataLog" />
+        <meta property="og:type" content="Website" />
+        <meta
+          name="image"
+          property="og:image"
+          content="https://i.ibb.co/xMYbyG2/Cata-Log-Welcome-Page.png"
+        />
+        <meta name="description" property="og:description" content="CataLog App" />
+        <meta name="author" content="Bawses Team" />
+      </Helmet>
+
+      <div>
+        <WelcomeSection
+          onPressCTA={() => router.push(isLoggedIn ? "/contacts" : "/login")}
+          CTAtitle={isLoggedIn ? "Go to your contacts" : "Login"}
+          btnLoading={btnLoading}
+        />
+        <ProfessionalsSection
+          isLoggedIn={isLoggedIn}
+          onPressCTA={() => router.push("/signup")}
+        />
+        <OrganisationsSection
+          isLoggedIn={isLoggedIn}
+          onPressCTA={() => router.push("/signup")}
+        />
+      </div>
+    </>
   );
 }
